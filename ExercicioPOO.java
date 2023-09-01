bf3bf52f57e3bdc8ec145ba12c8ac20852c317ed
Felipe Marques <114961292+felipmarques@users.noreply.github.com>
2023-09-01 19:03:06 -0300

    Update ExercicioPOO.java

--- ExercicioPOO.java
+++ ExercicioPOO.java
@@ -5,7 +5,7 @@ public class ExercicioPOO {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        Pessoa cliente1 = new Pessoa("Felipe", 20); // Assuming you have a Pessoa class
+        Pessoa cliente1 = new Pessoa("Felipe", 20); 
         
         cliente1.mostra_nome();
         cliente1.mostra_idade();
