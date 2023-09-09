c48788f80b0beae8705c986805b45464383983f4
Felipe Marques <114961292+felipmarques@users.noreply.github.com>
2023-09-09 20:33:08 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,22 @@
+package uscs;
+
+public class Carro {
+
+    public String marca;
+    public String modelo;
+    public int ano;
+
+    public void ligar(){
+        System.out.println("Carro ligado");
+    }
+    
+    public void acelerar(){
+        System.out.println("Acelerando");
+    }
+
+    public void frear(){
+        System.out.println("Freando");
+    }
+
+    
+}
