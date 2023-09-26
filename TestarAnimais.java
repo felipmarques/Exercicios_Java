7a66e951e654c96edbfa95950c536ef7e134e7a7
Felipe Marques <114961292+felipmarques@users.noreply.github.com>
2023-09-26 20:32:15 -0300

    Update TestarAnimais.java

--- TestarAnimais.java
+++ TestarAnimais.java
@@ -1,9 +1,13 @@
-package Animal;
+package animal;
 
-import java.util;
-public class Testar Animais{
+public class TestarAnimais {
 
     public static void main(String[] args) {
-
+        Mamifero camelo = new Mamifero("Camelo", 150.0f, 4, "Amarelo", "Terra", 2.0f, null);
+        Peixe tubarao = new Peixe("Tubarão", 300.0f, 0, "Cinzento", "Mar", 1.5f, "Barbatanas e cauda");
+        Mamifero urso = new Mamifero("Urso-do-Canadá", 180.0f, 4, "Vermelho", "Terra", 0.5f, "Mel");    
+        camelo.dadosMamifero();
+        tubarao.dados_peixe();
+        urso.dadosMamifero();
     }
 }
